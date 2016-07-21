e17e75d6b184aed2a1c70d0912714c3b1d60f1ee
bluemagma612 <bryce@bluemagma.com>
2016-07-21 17:38:17 -0500

    added mogolab creds

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	'database': 'mongodb://localhost/crm',
+	//'database': 'mongodb://localhost/crm',
+	'database': 'mongodb://bms-user-crm:10A974721931p8Ul@ds025232.mlab.com:25232/bms-user-crm',
 	'secret': 'N0way0ut'
 };
+ // heroku_m498r1zk 
+ // 62dE134I#4x9Eo!3P4W4
