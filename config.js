63b5264b4f982dc4127c79fcf26822fbc7cfbcec
bluemagma612 <bryce@bluemagma.com>
2016-08-15 15:03:10 -0500

    placeholder text for login

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	'port': process.env.PORT || 8080,
-	//'database': 'mongodb://localhost/crm',
-	'database': 'mongodb://bms-user-crm:10A974721931p8Ul@ds025232.mlab.com:25232/bms-user-crm',
-	'secret': 'N0way0ut'
-};
- // heroku_m498r1zk 
- // 62dE134I#4x9Eo!3P4W4
